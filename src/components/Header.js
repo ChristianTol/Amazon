8876fdb8c7a6a315a6f2b51550a99027f2a3bbcc
ChristianTol <christian.tol1998@hotmail.com>
2022-10-11 16:35:36 +0200

    Checkout page route and basic styling added.

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,15 +5,18 @@ import {
   ShoppingCartIcon,
 } from "@heroicons/react/outline";
 import { signIn, signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
 function Header() {
   const { data: session } = useSession();
+  const router = useRouter();
 
   return (
     <header className="sticky top-0 z-50">
       <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
           <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
               <Image
+                onClick={() => router.push("/")}
                 src="https://links.papareact.com/f90"
                 width={150}
                 height={40}
@@ -39,11 +42,13 @@ function Header() {
               </p>
               <p className="font-extrabold md:text-sm">Acount & Lists</p>
             </div>
+
             <div className="link">
               <p>Returns</p>
               <p className="font-extrabold md:text-sm">& Orders</p>
             </div>
-            <div className="relative link flex items-center">
+
+            <div onClick={() => router.push("/checkout")} className="relative link flex items-center">
               <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">0</span>
 
               <ShoppingCartIcon className="h-10" />
