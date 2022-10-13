8d910b0db9414d5a36d876dd146005544b01e537
ChristianTol <christian.tol1998@hotmail.com>
2022-10-13 02:33:39 +0200

    Firebase files added.

--- firebase.js
+++ firebase.js
@@ -1,2 +1,18 @@
 import firebase from "firebase";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAPZTnSldBk0-Ep4xzveaHNmh6-uGMHN6c",
+  authDomain: "fir-4686b.firebaseapp.com",
+  projectId: "fir-4686b",
+  storageBucket: "fir-4686b.appspot.com",
+  messagingSenderId: "1076570922446",
+  appId: "1:1076570922446:web:ff0b9326841a971f0d5f9f"
+};
+
+const app = !firebase.apps.length 
+  ? firebase.initializeApp(firebaseConfig) 
+  : firebase.app();
+
+const db = app.firestore();
+
+export default db;
